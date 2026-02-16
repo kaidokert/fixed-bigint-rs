f53f93843a748cd35ea151a2c821dde2c4967a20
Kaido Kert <kaidokert@gmail.com>
2026-02-15 20:55:07 -0800

    Address PR feedback: direct ops, add rem helpers

--- src/fixeduint/euclid.rs
+++ src/fixeduint/euclid.rs
@@ -1,7 +1,7 @@
 use num_traits::{CheckedEuclid, Euclid};
 
 use super::{FixedUInt, MachineWord};
-use crate::const_numtrait::{ConstCheckedDiv, ConstCheckedEuclid, ConstCheckedRem, ConstEuclid};
+use crate::const_numtrait::{ConstCheckedEuclid, ConstEuclid, ConstZero};
 use crate::machineword::ConstMachineWord;
 
 c0nst::c0nst! {
@@ -19,34 +19,42 @@ c0nst::c0nst! {
 
     impl<T: [c0nst] ConstMachineWord + MachineWord, const N: usize> c0nst ConstCheckedEuclid for FixedUInt<T, N> {
         fn checked_div_euclid(&self, v: &Self) -> Option<Self> {
-            ConstCheckedDiv::checked_div(self, v)
+            if v.is_zero() {
+                None
+            } else {
+                Some(*self / *v)
+            }
         }
 
         fn checked_rem_euclid(&self, v: &Self) -> Option<Self> {
-            ConstCheckedRem::checked_rem(self, v)
+            if v.is_zero() {
+                None
+            } else {
+                Some(*self % *v)
+            }
         }
     }
 }
 
-// num_traits::Euclid - delegates to ConstEuclid
+// num_traits::Euclid - uses direct operators (no const bounds needed)
 impl<T: MachineWord, const N: usize> Euclid for FixedUInt<T, N> {
     fn div_euclid(&self, v: &Self) -> Self {
-        ConstEuclid::div_euclid(self, v)
+        self / v
     }
 
     fn rem_euclid(&self, v: &Self) -> Self {
-        ConstEuclid::rem_euclid(self, v)
+        self % v
     }
 }
 
-// num_traits::CheckedEuclid - delegates to ConstCheckedEuclid
+// num_traits::CheckedEuclid - uses direct checked calls (no const bounds needed)
 impl<T: MachineWord, const N: usize> CheckedEuclid for FixedUInt<T, N> {
     fn checked_div_euclid(&self, v: &Self) -> Option<Self> {
-        ConstCheckedEuclid::checked_div_euclid(self, v)
+        num_traits::CheckedDiv::checked_div(self, v)
     }
 
     fn checked_rem_euclid(&self, v: &Self) -> Option<Self> {
-        ConstCheckedEuclid::checked_rem_euclid(self, v)
+        num_traits::CheckedRem::checked_rem(self, v)
     }
 }
 
@@ -86,11 +94,25 @@ mod tests {
             ConstEuclid::div_euclid(a, b)
         }
 
+        pub c0nst fn const_rem_euclid<T: [c0nst] ConstMachineWord + MachineWord, const N: usize>(
+            a: &FixedUInt<T, N>,
+            b: &FixedUInt<T, N>,
+        ) -> FixedUInt<T, N> {
+            ConstEuclid::rem_euclid(a, b)
+        }
+
         pub c0nst fn const_checked_div_euclid<T: [c0nst] ConstMachineWord + MachineWord, const N: usize>(
             a: &FixedUInt<T, N>,
             b: &FixedUInt<T, N>,
         ) -> Option<FixedUInt<T, N>> {
             ConstCheckedEuclid::checked_div_euclid(a, b)
+        }
+
+        pub c0nst fn const_checked_rem_euclid<T: [c0nst] ConstMachineWord + MachineWord, const N: usize>(
+            a: &FixedUInt<T, N>,
+            b: &FixedUInt<T, N>,
+        ) -> Option<FixedUInt<T, N>> {
+            ConstCheckedEuclid::checked_rem_euclid(a, b)
         }
     }
 
@@ -99,16 +121,22 @@ mod tests {
         let a = FixedUInt::<u8, 2>::from(100u8);
         let b = FixedUInt::<u8, 2>::from(30u8);
         assert_eq!(const_div_euclid(&a, &b), 3u8.into());
+        assert_eq!(const_rem_euclid(&a, &b), 10u8.into());
         assert_eq!(const_checked_div_euclid(&a, &b), Some(3u8.into()));
+        assert_eq!(const_checked_rem_euclid(&a, &b), Some(10u8.into()));
 
         #[cfg(feature = "nightly")]
         {
             const A: FixedUInt<u8, 2> = FixedUInt { array: [100, 0] };
             const B: FixedUInt<u8, 2> = FixedUInt { array: [30, 0] };
             const DIV_RESULT: FixedUInt<u8, 2> = const_div_euclid(&A, &B);
-            const CHECKED_RESULT: Option<FixedUInt<u8, 2>> = const_checked_div_euclid(&A, &B);
+            const REM_RESULT: FixedUInt<u8, 2> = const_rem_euclid(&A, &B);
+            const CHECKED_DIV: Option<FixedUInt<u8, 2>> = const_checked_div_euclid(&A, &B);
+            const CHECKED_REM: Option<FixedUInt<u8, 2>> = const_checked_rem_euclid(&A, &B);
             assert_eq!(DIV_RESULT, FixedUInt { array: [3, 0] });
-            assert_eq!(CHECKED_RESULT, Some(FixedUInt { array: [3, 0] }));
+            assert_eq!(REM_RESULT, FixedUInt { array: [10, 0] });
+            assert_eq!(CHECKED_DIV, Some(FixedUInt { array: [3, 0] }));
+            assert_eq!(CHECKED_REM, Some(FixedUInt { array: [10, 0] }));
         }
     }
 }
