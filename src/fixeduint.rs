2465798d1ab700c9b610c907a0c779afc4d3bf28
Hennadii Chernyshchyk <genaloner@gmail.com>
2026-02-20 07:00:41 +0200

    Add constructor from array (#95)
    * Add constructor from array
    
    * Add getter for `array`
    
    * Rename `bytes` into `words`

--- src/fixeduint.rs
+++ src/fixeduint.rs
@@ -66,6 +66,15 @@ where
 #[cfg(feature = "zeroize")]
 impl<T: MachineWord, const N: usize> DefaultIsZeroes for FixedUInt<T, N> {}
 
+impl<T, const N: usize> From<[T; N]> for FixedUInt<T, N>
+where
+    T: MachineWord,
+{
+    fn from(array: [T; N]) -> Self {
+        Self { array }
+    }
+}
+
 const LONGEST_WORD_IN_BITS: usize = 128;
 
 impl<T: MachineWord, const N: usize> FixedUInt<T, N> {
@@ -79,6 +88,11 @@ impl<T: MachineWord, const N: usize> FixedUInt<T, N> {
         FixedUInt {
             array: [T::zero(); N],
         }
+    }
+
+    /// Returns the underlying array.
+    pub fn words(&self) -> &[T; N] {
+        &self.array
     }
 
     /// Returns number of used bits.
